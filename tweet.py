c57fa3c825c8883913393b6e29d7cbb0fdd01420
mio256 <reiya@woorth.io>
2022-10-18 10:08:34 +0900

    :sparkles: tweet.py for tweet "hello"

---
+++ tweet.py
@@ -0,0 +1,12 @@
+import os
+import tweepy
+
+client = tweepy.Client(
+    os.environ['BEARER_TOKEN'],
+    os.environ['CONSUMER_KEY'],
+    os.environ['CONSUMER_SECRET'],
+    os.environ['ACCESS_TOKEN'],
+    os.environ['ACCESS_TOKEN_SECRET']
+)
+
+client.create_tweet(text='hello')

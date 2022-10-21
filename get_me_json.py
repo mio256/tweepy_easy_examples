034322db30bc9db57c1a2b9875dd55f24fd428ee
mio256 <reiya@woorth.io>
2022-10-21 13:57:12 +0900

    :sparkles: get_me_json.py

---
+++ get_me_json.py
@@ -0,0 +1,18 @@
+import os
+import json
+import pprint
+import tweepy
+from requests import Response
+
+client = tweepy.Client(
+    os.environ['BEARER_TOKEN'],
+    os.environ['CONSUMER_KEY'],
+    os.environ['CONSUMER_SECRET'],
+    os.environ['ACCESS_TOKEN'],
+    os.environ['ACCESS_TOKEN_SECRET'],
+    return_type=Response
+)
+
+response = client.get_me().json()
+
+pprint.pprint(response)
